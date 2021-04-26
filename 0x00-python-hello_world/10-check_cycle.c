0a67bb244b98e4dde4723f362fa56b051c050190
Sebas93cay <sebas93cay@gmail.com>
2021-04-26 22:53:05 +0000

    Fix 3, 8 and add 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,8 +2,24 @@
 
 int check_cycle(listint_t *list)
 {
-	int checked_nodes = 0;
-	listint_t head
-	
-	
+	int checked_nodes = 0, i = 0;
+	listint_t *node, *node_checked;
+
+	if (list == NULL)
+		return (0);
+
+	node = list;
+	while (node != NULL)
+	{
+		node_checked = list;
+		for (i = 0; i < checked_nodes; i++)
+		{
+			if (node == node_checked)
+				return (1);
+			node_checked = node_checked->next;
+		}
+		checked_nodes++;
+		node = node->next;
+	}
+	return (0);
 }
