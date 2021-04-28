e2f3b9909418c9fa76b985bd4042c2004b7dfd4a
Sebas93cay <sebas93cay@gmail.com>
2021-04-28 17:22:05 +0000

    0 to 9

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+
+    from sys import argv
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    if argv[2] not in ['+', '-', '*', '/']:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    from calculator_1 import add, sub, mul, div
+    
+    switch_operador = {
+        '+': add,
+        '-': sub,
+        '*': mul,
+        '/': div
+    }
+    r = switch_operador.get(argv[2])(int(argv[1]), int(argv[3]))
+    print("{} {} {} = {}".format(argv[1], argv[2], argv[3], r))
---
+++ 0x02-python-import_modules/importing.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+__import__("0-add")
+__import__("1-calculation")
+
+
