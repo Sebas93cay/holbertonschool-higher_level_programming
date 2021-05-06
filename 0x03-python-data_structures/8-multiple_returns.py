464e4412df4ea843f67060041fa568ff413ae6a5
Sebastian Asus Elementary <sebas93cay@gmail.com>
2021-05-06 00:38:03 +0000

    Fix 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
+def multiple_returns(sentence=None):
     if (sentence is None):
+        return 0, None
+    length = len(sentence)
+    if (length == 0):
         return 0, None
     return len(sentence), sentence[0]
