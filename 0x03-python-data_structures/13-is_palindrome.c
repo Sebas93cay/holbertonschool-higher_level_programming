557c2fc75167bccd4acb5a73353201e6a080ff40
Sebastian Asus Elementary <sebas93cay@gmail.com>
2021-05-06 00:03:34 +0000

    Fix 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,44 +3,23 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the end of a listint_t list
- * @head: pointer to pointer of first node of listint_t list
- * @n: integer to be included in new node
- * Return: address of the new element or NULL if it fails
+ * listint_len - Returns the lenght of listint_t
+ * @head: pointer of first node of listint_t list
+ * Return:  Returns the lenght of listint_t
  */
-
-listint_t *add_node(listint_t **head, const int n)
+int listint_len(listint_t *head)
 {
-	listint_t *new = NULL;
+	int len = 0;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->next = *head;
-	*head = new;
-
-	return (new);
+	while (head != NULL)
+	{
+		head = head->next;
+		len++;
+	}
+	return (len);
 }
 
-/**
- * list_compare - compare if two linked list listint_t are equal
- * @a: first list
- * @b: second list
- * Return: 1 if list is a palindrome, 0 otherwise
- */
-int list_compare(listint_t *a, listint_t *b)
-{
-	if (a == NULL && b == NULL)
-		return (1);
-	if (a == NULL || b == NULL)
-		return (0);
-	if (a->n != b->n)
-		return (0);
-	else
-		return (list_compare(a->next, b->next));
-}
+
 
 /**
  * is_palindrome - compare if a listint_t is palindrome
@@ -49,19 +28,41 @@ int list_compare(listint_t *a, listint_t *b)
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *reverted = NULL;
+	int *nums = NULL, *revnums = NULL;
 	listint_t *tmp_node = NULL;
-	int equal = 0;
+	int equal = 1, len = 0, i = 0;
+
+	len = listint_len(*head);
+
+	if (len == 0)
+		return (1);
+
+	nums = malloc(sizeof(int) * len);
+	if (nums == NULL)
+		return (0);
+	revnums = malloc(sizeof(int) * len);
+	if (revnums == NULL)
+	{
+		free(nums);
+		return (0);
+	}
 
 	tmp_node = *head;
 	while (tmp_node != NULL)
 	{
-		add_node(&reverted, tmp_node->n);
+		nums[i] = tmp_node->n;
+		i++;
 		tmp_node = tmp_node->next;
 	}
-	equal = list_compare(*head, reverted);
-	free_listint(reverted);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		if (nums[i] != nums[len - 1 - i])
+		{
+			equal = 0;
+			break;
+		}
+	}
+	free(nums);
 	return (equal);
-
-
 }
