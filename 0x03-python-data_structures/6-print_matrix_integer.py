583cb1ee3d4ea81f8b29a547b78cd592fa94ca00
Sebastian Asus Elementary <sebas93cay@gmail.com>
2021-05-06 00:22:06 +0000

    Fix 3 and 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         last_idx = len(x) - 1
         for y in x:
-            print("{}".format(y), end=(' ' if counter < last_idx else ''))
+            print("{:d}".format(y), end=(' ' if counter < last_idx else ''))
             counter += 1
         print('')
         counter = 0
