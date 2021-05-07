f1b104a0920def000251e0e6a3fce806fa627716
Sebastian-Vostro <sebas93cay@gmail.com>
2021-05-06 19:12:41 -0500

    Fix 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is None:
+        return None
     letras = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     numero = 0
     s = roman_string
