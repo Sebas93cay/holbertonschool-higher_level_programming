d90330c3b2dd1d4c4570390b5ca4ff040000c071
Seb Asus VS <sebas93cay@gmail.com>
2021-05-17 13:02:07 -0500

    Fix 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+    if value is None:
+        return False
     ret = True
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except:
         ret = False
     return ret
