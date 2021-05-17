c5e63aa497d850c5df99fe0f4dad8a978be64177
Seb Asus VS <sebas93cay@gmail.com>
2021-05-17 17:43:06 -0500

    Answer 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,4 +1,9 @@
-def lafunc(a, b):
-    r = a + b
-
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        r = a / b
+    except ZeroDivisionError:
+        r = None
+    finally:
+        print("Inside result: {}".format(r))
     return r
