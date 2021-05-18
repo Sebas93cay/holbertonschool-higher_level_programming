04fac1b61f1f0da9ee38e84b63843e1be51b2a9a
Seb Asus VS <sebas93cay@gmail.com>
2021-05-17 21:20:53 -0500

    Answer 5 and 6 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+
+    if value is None:
+        return False
+    printed = True
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as er:
+        sys.stderr.write(er.args[0] + "\n")
+        printed = False
+    return printed
