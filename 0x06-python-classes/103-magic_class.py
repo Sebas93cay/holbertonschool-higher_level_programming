3d08490bf3156186e07e06478d44f76299cfd877
Seb Asus VS <sebas93cay@gmail.com>
2021-05-19 00:04:29 -0500

    Fix 103

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -2,23 +2,20 @@
 """magic class to create from its bycodes"""
 
 
-import dis
-
 import math
 
 
 class MagicClass:
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         self.__radius = 0
         if (type(radius) is not int and type(radius) is not float):
             raise TypeError("radius must must be a number")
         self.__radius = radius
 
     def area(self):
-        return self.__radius ** 2 * math.pi
+        """find area"""
+        return (self.__radius ** 2) * math.pi
 
     def circumference(self):
-        return 2 * math.pi * self.__radius
-
-
-dis.dis(MagicClass)
+        """find circumference"""
+        return (2 * math.pi) * self.__radius
