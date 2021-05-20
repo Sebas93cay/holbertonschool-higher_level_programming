866f52ec33473eedb29f253f23a0b100f46a1856
Seb Asus VS <sebas93cay@gmail.com>
2021-05-20 18:22:37 -0500

    Fix 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,7 +5,10 @@
 class Rectangle:
     """Rectangle class"""
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
+        Rectangle.number_of_instances += 1
         self.width = width
         self.height = height
 
@@ -53,4 +56,5 @@ class Rectangle:
             str(self.__height)+")"
 
     def __del__(self):
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
