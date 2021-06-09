621bef6aa03b33ea980e119be24f14f3cfd45fc3
Seb Asus VS <sebas93cay@gmail.com>
2021-06-08 21:00:47 -0500

    Start of kathes base test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
-"""This module has the test for the base class"""
-from models.base import Base
-from models.square import Square
-from models.rectangle import Rectangle
+"""
+Test differents behaviors of the Base class
+"""
 import unittest
+import pep8
 import os
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
-class testBase(unittest.TestCase):
-    """test for base class"""
+class TestBase(unittest.TestCase):
+    """
+    A class to test Base Class
+    """
 
-    def setUp(self):
-        """set up for every test"""
-        super().setUp()
-        Base._Base__nb_objects = 0
+    def test_pep8_base(self):
+        """
+        Test that checks PEP8
+        """
+        syntax = pep8.StyleGuide(quit=True)
+        check = syntax.check_files(['models/base.py'])
+        self.assertEqual(
+            check.total_errors, 0,
+            "Found code style errors (and warnings)."
+        )
 
-    def test_init(self):
-        """test init"""
-        b1 = Base()
-        b2 = Base()
-        self.assertEqual(1, b1.id)
-        self.assertEqual(b2.id, 2)
-        b3 = Base()
-        b4 = Base(34)
-        b5 = Base()
-        self.assertEqual(b3.id, 3)
-        self.assertEqual(b4.id, 34)
-        self.assertEqual(b5.id, 4)
+    def test_id_as_positive(self):
+        """
+        Test for positive Base Class id
+        """
+        base_instance = Base(110)
+        self.assertEqual(base_instance.id, 110)
+        base_instance = Base(30)
+        self.assertEqual(base_instance.id, 30)
