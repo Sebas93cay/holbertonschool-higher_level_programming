bfd90744b91aa8ee238f7120990d238573622841
Seb Asus VS <sebas93cay@gmail.com>
2021-07-12 22:40:52 -0500

    Fix mistakes

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
-- inserts a NEW ROW IN the TABLE first_table (DATABASE hbtn_0c_0)
+-- inserts a NEW ROW IN the TABLE first_table (DATABASE hbtn_0c_0)
 INSERT INTO `first_table` (`id`, `name`)
 VALUES (89, "Holberton School");
