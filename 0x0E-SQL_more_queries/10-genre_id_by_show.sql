69693712ff4a343267faf0a3f3a91fda9e6bb514
Seb Asus VS <sebas93cay@gmail.com>
2021-07-13 23:57:37 -0500

    Answer 11

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,4 @@
 -- second line
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
-INNER JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id;
