dc495f87dcb90a79b0438b1d19a0d83881d2c59b
Seb Asus VS <sebas93cay@gmail.com>
2021-08-02 18:55:51 -0500

    Task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const {
+  argv
+} = require('process');
+argv[2] = argv[2].trim();
+if (/^-?\d*\.?\d*$/.test(argv[2])) {
+  console.log(argv[2].match(/^-?\d+/)[0]);
+} else {
+  console.log('Not a number');
+}
