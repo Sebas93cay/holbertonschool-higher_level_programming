0cca196b63b190b63c119b8f14c444dc18510e85
Seb Asus VS <sebas93cay@gmail.com>
2021-08-02 18:17:32 -0500

    Fix 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 
-const argv = require('process').argv;
+const {
+  argv
+} = require('process');
 
-if (!argv[2]) {
+if (argv[2] === undefined) {
   console.log('No argument');
 } else {
   for (const arg of argv.slice(2)) {
