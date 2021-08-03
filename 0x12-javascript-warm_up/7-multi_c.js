d9cb791b8410ad92a0d871abd2a5a7bde4198a80
Seb Asus VS <sebas93cay@gmail.com>
2021-08-02 19:24:31 -0500

    Task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const argv = require('process').argv;
+
+if (Number(argv[2])) {
+  for (let i = 0; i < parseInt(argv[2]); i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
