d04ffee58bcfecd6e4f2eceab001538ed36da1b5
Seb Asus VS <sebas93cay@gmail.com>
2021-08-03 16:07:31 -0500

    Fix 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,9 +3,9 @@
 exports.esrever = function (list) {
   const l = list.length;
   const listC = list.slice(0);
-  for (let i = 0; i < l; i++) {
+  for (let i = 0; i < Math.floor(l / 2); i++) {
     const tmp = listC[l - i - 1];
-    listC[l - i] = listC[i];
+    listC[l - i - 1] = listC[i];
     listC[i] = tmp;
   }
   return listC;
