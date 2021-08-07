7fb8a150795c14b35c4f7014e1201d2fd599510e
Seb Asus VS <sebas93cay@gmail.com>
2021-08-07 18:22:20 -0500

    Tasks 9 and 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module prints the state give as a parameter
+"""
+
+import sys
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    if (len(sys.argv) == 5):
+        user, password, database, state_name = (sys.argv[1],
+                                                sys.argv[2], sys.argv[3], sys.argv[4])
+        engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                user, password, database))
+        SessionMkr = sessionmaker(bind=engine)
+        session = SessionMkr()
+        state = session.query(State).filter(State.name == state_name).first()
+        if (state):
+            print(state.id)
+        else:
+            print('Not found')
