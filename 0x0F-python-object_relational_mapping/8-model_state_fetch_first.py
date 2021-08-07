0198d4c1c24e094ea83e8c23c30841d54c6d636e
Seb Asus VS <sebas93cay@gmail.com>
2021-08-07 17:25:32 -0500

    Task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module prints the first state using SWLAlchemy
+"""
+import MySQLdb
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    if (len(sys.argv) == 4):
+        user, password, database = (sys.argv[1], sys.argv[2], sys.argv[3])
+        engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                user, password, database))
+        SessionMkr = sessionmaker(bind=engine)
+        session = SessionMkr()
+        state = session.query(State).first()
+        print("{}: {}".format(state.id, state.name))
