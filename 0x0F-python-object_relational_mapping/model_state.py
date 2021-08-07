e13e2544eddbfb692d0b0e90b3d0cfc0acc2d5ad
Seb Asus VS <sebas93cay@gmail.com>
2021-08-07 13:44:25 -0500

    Add documentation to task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,6 +10,9 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    State Class
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
