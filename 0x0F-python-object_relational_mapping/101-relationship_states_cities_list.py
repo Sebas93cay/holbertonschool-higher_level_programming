4b40bef7828e3f93318e0989ffb7be2a3c5e06d8
Seb Asus VS <sebas93cay@gmail.com>
2021-08-09 11:05:48 -0500

    Revert "Afredo 101"
    This reverts commit 85cf399e9891c9aa31018448145b781ab5bd2d55.

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,50 +1,26 @@
-# #!/usr/bin/python3
-# """
-# This module prints the first state using SWLAlchemy
-# """
-# import sys
-# from relationship_state import State, Base
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from relationship_city import City
-
-
-# if __name__ == '__main__':
-# if (len(sys.argv) == 4):
-# user, password, database = (sys.argv[1],
-# sys.argv[2], sys.argv[3])
-# engine = create_engine(
-# 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-# user, password, database), pool_pre_ping=True)
-# SessionMkr = sessionmaker(bind=engine)
-# session = SessionMkr()
-# states = session.query(State).order_by(State.id).all()
-# for s in states:
-# print("{}: {}".format(s.id, s.name))
-# for c in s.cities:
-# print("    {}: {}".format(c.id, c.name))
-# session.close()
-
 #!/usr/bin/python3
-"""Script that lists all States with their City names and ids
-from database hbtn_0e_101_usa."""
-from sys import argv
-from relationship_state import State
-from relationship_city import City
+"""
+This module prints the first state using SWLAlchemy
+"""
+import sys
+from relationship_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_city import City
 
-if __name__ == "__main__":
-    user, password, dbname = argv[1], argv[2], argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(user, password, dbname), pool_pre_ping=True)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    for state in session.query(State).order_by(State.id):
-        print("{:d}: {}".format(state.id, state.name))
-        for city in state.cities:
-            print("    {:d}: {}".format(city.id, city.name))
-
-    session.close()
+if __name__ == '__main__':
+    if (len(sys.argv) == 4):
+        user, password, database = (sys.argv[1],
+                                    sys.argv[2], sys.argv[3])
+        engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                user, password, database), pool_pre_ping=True)
+        SessionMkr = sessionmaker(bind=engine)
+        session = SessionMkr()
+        states = session.query(State).order_by(State.id).all()
+        for s in states:
+            print("{}: {}".format(s.id, s.name))
+            for c in s.cities:
+                print("    {}: {}".format(c.id, c.name))
+        session.close()
