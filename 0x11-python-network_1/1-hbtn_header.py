291bde517ce53784556aa3f3c1df926c7660038e
Seb Asus VS <sebas93cay@gmail.com>
2021-08-20 12:47:41 -0500

    Task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-#This script that fetches https://intranet.hbtn.io/status
+# This script that fetches
 
 import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status ') as response:
-    html = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode()))
+from sys import argv
+with urllib.request.urlopen(argv[1]) as response:
+    for header in response.getheaders():
+        if header[0] == 'X-Request-Id':
+            print(header[1])
+            break
