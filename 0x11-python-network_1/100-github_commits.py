c07e1ba903437e0b199d8d8dcd6b0e3f9c24f91f
Seb Asus VS <sebas93cay@gmail.com>
2021-08-20 19:09:03 -0500

    Fix 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,11 +6,10 @@ list 10 commits (from the most recent to oldest) of the given repository
 import requests
 import sys
 
-repo = sys.argv[1]
-user = sys.argv[2]
-
 
 if __name__ == '__main__':
+    repo = sys.argv[1]
+    user = sys.argv[2]
     r = requests.get(
         "https://api.github.com/repos/{}/{}/commits".format(user, repo))
     commits = r.json()[:10]
